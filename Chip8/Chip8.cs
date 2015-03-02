007d5c379e565c2c61f2c4e55e916c516a6d7bb8
Evan McIntire <mcintire.evan@gmail.com>
2015-03-02 16:51:24 -0500

    Implement fonts and drawing, along with several more opcodes

--- Chip8/Chip8.cs
+++ Chip8/Chip8.cs
@@ -24,6 +24,11 @@ namespace Chip8
         byte[,] display;
         bool redraw;
 
+        ushort delay;
+        ushort soundDelay;
+
+        ushort[] font;
+
         public Chip8()
             : base()
         {
@@ -41,7 +46,34 @@ namespace Chip8
             stackPointer = 0;
 
             display = new byte[64,32];
-            redraw = true; 
+            redraw = true;
+
+            delay = 0;
+            soundDelay = 0;
+
+            font = new ushort[]{ 
+                0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+                0x20, 0x60, 0x20, 0x20, 0x70, // 1
+                0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+                0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+                0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+                0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+                0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+                0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+                0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+                0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+                0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+                0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+                0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+                0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+                0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+                0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+            };
+
+            for (int i = 0; i < font.Length; i++)
+            {
+                memory[0x50 + i] = font[i];
+            }
         }
 
         private void LoadFile(string file) 
@@ -84,15 +116,36 @@ namespace Chip8
             //The second part ORs the second half of the opcode on, leaving us with 0xXXXX
             ushort opcode = (ushort)(memory[counter] << 8 | memory[counter + 1]);
 
-            Console.WriteLine((opcode & 0xFFFF).ToString("x"));
+            Console.WriteLine(counter + "::" + (opcode & 0xFFFF).ToString("x"));
             switch (opcode & 0xF000)
             {
                 case 0x0000:
                     {
+                        switch (opcode & 0x00FF)
+                        {
+                            case 0x00E0:
+                                {
+                                    display = new byte[64, 32];
+                                    break;
+                                }
+                            case 0x00EE:
+                                {
+                                    stackPointer--;
+                                    counter = (ushort)(stack[stackPointer] + 2);
+                                    break;
+                                }
+                            default:
+                                {
+                                    Exit();
+                                    break;
+                                }
+                        }
                         break;
                     }
                 case 0x1000:
                     {
+                        int address = opcode & 0x0FFF;
+                        counter = (ushort)address;
                         break;
                     }
                 case 0x2000: //Call subroutine at 0x2NNN
@@ -106,6 +159,16 @@ namespace Chip8
                     }
                 case 0x3000:
                     {
+                        int x = (opcode & 0x0F00) >> 8;
+                        int nn = (opcode & 0x00FF);
+                        if (registers[x] == nn)
+                        {
+                            counter += 4;
+                        }
+                        else
+                        {
+                            counter += 2;
+                        }
                         break;
                     }
                 case 0x4000:
@@ -126,6 +189,10 @@ namespace Chip8
                     }
                 case 0x7000:
                     {
+                        int x = (opcode & 0x0F00) >> 8;
+                        ushort nn = (ushort)(opcode & 0x00FF);
+                        registers[x] += nn;
+                        counter += 2;
                         break;
                     }
                 case 0x8000:
@@ -134,7 +201,6 @@ namespace Chip8
                     }
                 case 0x9000:
                     {
-                        Console.WriteLine("9");
                         break;
                     }
                 case 0xA000:
@@ -154,6 +220,26 @@ namespace Chip8
                 case 0xD000:
                     {
                         //Draw at DXYN
+                        int x = registers[(opcode & 0x0F00) >> 8];
+                        int y = registers[(opcode & 0x00F0) >> 4];
+                        int height = opcode & 0x000F;
+
+                        registers[0xF] = 0;
+
+                        for(int _y = 0; _y < height; _y++)
+                        {
+                            ushort pixel = memory[addressPointer + _y];
+                            for (int _x = 0; _x < 8; _x++)
+                            {
+                                if((pixel & (0x80 >> _x)) != 0)
+                                {
+                                    if (display[x + _x, y + _y] == 1)
+                                        registers[0xF] = 1;
+                                    display[x + _x, y + _y] ^= 1;
+                                }
+                            }
+                        }
+
                         counter += 2;
                         break;
                     }
@@ -163,6 +249,65 @@ namespace Chip8
                     }
                 case 0xF000:
                     {
+                        switch(opcode & 0x00FF) 
+                        {
+                            case 0x0007:
+                                {
+                                    int x = opcode & 0x0F00 >> 8;
+                                    registers[x] = delay;
+                                    counter += 2;
+                                    break;
+                                }
+                            case 0x0015:
+                                {
+                                    int x = opcode & 0x0F00 >> 8;
+                                    delay = registers[x];
+                                    counter += 2;
+                                    break;
+                                }
+                            case 0x0018:
+                                {
+                                    int x = opcode & 0x0F00 >> 8;
+                                    soundDelay = registers[x];
+                                    counter += 2;
+                                    break;
+                                }
+                            case 0x0029:
+                                {
+                                    int x = opcode & 0x0F00 >> 8;
+                                    int character = registers[x];
+                                    //Console.WriteLine(character);
+                                    //Console.WriteLine((ushort)(0x050 + (character * 5)));
+                                    addressPointer = (ushort)(0x050 + (character * 5));
+                                    counter += 2;
+                                    break;
+                                }
+                            case 0x0033:
+                                {
+                                    int vx = registers[opcode & 0x0F00 >> 8], hundreds, tens;
+                                    hundreds = ((vx - (vx % 100)) / 100);
+                                    vx -= hundreds * 100;
+                                    tens = ((vx - (vx % 10)) / 10);
+                                    vx -= tens * 10;
+
+                                    memory[addressPointer] = (ushort)hundreds;
+                                    memory[addressPointer + 1] = (ushort)tens;
+                                    memory[addressPointer + 2] = (ushort)vx;
+                                    counter += 2;
+                                    break;
+                                }
+                            case 0x0065:
+                                {
+                                    int x = opcode & 0x0F00 >> 8;
+                                    for (int i = 0; i < x; i++)
+                                    {
+                                        registers[i] = memory[addressPointer + 1];
+                                    }
+                                    addressPointer += (ushort)(x + 1);
+                                    counter += 2;
+                                    break;
+                                }
+                        }
                         break;
                     }
                 default:
@@ -183,7 +328,7 @@ namespace Chip8
                 for (int j = 0; j < 32; j++)
                 {
                     Color color = display[i,j] == 1 ? Color.White : Color.Black;
-                    spriteBatch.Draw(pixel, new Rectangle(i * 64 * 16, i * 32 * 16, 64 * 16, 32 * 16), color);
+                    spriteBatch.Draw(pixel, new Rectangle(i * 16, j * 16, 16, 16), color);
                 }
             }
             spriteBatch.End();
